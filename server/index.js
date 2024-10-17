4922e27d9d701f6d5a03eca57bb924047582dc79
KAUÃ FONSECA VIEIRA <kaua_vieira@estudante.sesisenai.org.br>
2024-10-17 11:56:05 -0300

    first

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import Express from 'express'
+
+const app = Express()
+
+app.get('/pegar', function (req, res){
+    res.send('enviar esta mensagem')
+})
+app.get('/pegaroutracoisa', function(req, res){
+    res.send('esta e a outra mensagem')
+})
+app.listen(8000)
